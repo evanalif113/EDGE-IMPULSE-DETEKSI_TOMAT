7afc18b4b96b1f1064f5930cca83349541c08bb8
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-08-30 12:13:37 +0700

    Reduce XCLK frequency to 10MHz for improved camera performance in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ static camera_config_t camera_config = {
     .pin_pclk = PCLK_GPIO_NUM,
 
     //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-    .xclk_freq_hz = 20000000,
+    .xclk_freq_hz = 10000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 
